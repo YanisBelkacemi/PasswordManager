caebf34e5132bbd7e31f7cbea76f989b7eb7f241
YanisBelkacemi <108769998+YanisBelkacemi@users.noreply.github.com>
2022-07-06 14:06:10 -0400

    Add files via upload

--- proxy.py
+++ proxy.py
@@ -1,9 +1,13 @@
 import json
 
-#where our log-in info are stored
 accounts_data = dict()
-accounts_data["yanis"] = "Yanis12345678yanis"
-accounts_data["rayan"] = "Rayan12345678rayan"
+
+#add new users
+def dumper():
+    with open("user_info.json", "w") as user:
+        json.dump(accounts_data, user, indent=2)
+with open("user_info.json", "r") as password:
+    user_info = json.load(password)
 
 
 
@@ -16,8 +20,11 @@ doYou = input("do you have an account (Y/N) : ")
 
 #DataBase That the user wants to access as json
 
-with open("data.json","r") as f:
-    data = json.load(f)
+with open("data.json", "r") as f:
+        data = json.load(f)
+
+
+
 
 #check if you have an account or this is your first time
 def check_for_account():
@@ -25,8 +32,8 @@ def check_for_account():
         login()
     if doYou == 'n':
         sign_in()
-    else:
-        print("I cant reconize your command ")
+
+
 
 
 
@@ -34,12 +41,20 @@ def check_for_account():
 
 #sign-in
 def sign_in():
+
+
     sign_name = input('please Enter the name that you want to assign for your account : ')
     sign_password = input('please enter the password that you want to assign to your account : ')
-    if sign_name not in accounts_data:
+
+
+
+    if sign_name not in user_info:
         accounts_data[sign_name] = sign_password
+        dumper()
         print("your account has been created!")
-        login()
+
+
+
     else:
         print("choose another name ")
         sign_in()
@@ -56,26 +71,24 @@ def login():
     _accname = input("enter your name : ")
     _accpassword = input("enter your password : ")
 
+
 #check if the entered login info are the same with those in the login database
-    if _accname in accounts_data.keys() and _accpassword in accounts_data.values():
-        if _accname in data:
-            print(data[_accname])
-        else:
-            print("we cant find data with name:" , _accname)
+    if _accname in user_info:
+        intent_data= json.dumps(data , indent=2)
+        print(intent_data)
+
 
 #if the name is incorrect but the password is correct
-    elif _accname not in accounts_data.keys():
+    elif _accname not in user_info.keys():
 
         print('your name is incorrect please Retry !')
+
 
 #if the password is incorrect but the name is correct
-    elif _accpassword not in accounts_data.keys():
+    elif _accpassword not in user_info.keys():
 
         print('your name is incorrect please Retry !')
-
-
-
-
 
 
 check_for_account()
+dumper()
